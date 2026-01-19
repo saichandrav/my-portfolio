3d54fc7d6a514075210a92c7aa71d20578812275
Chandu VK <chanduvinnakota26@gmail.com>
2026-01-19 22:50:11 +0530

    updated profile links

--- Frontend/myportfolio/src/sections/Home.jsx
+++ Frontend/myportfolio/src/sections/Home.jsx
@@ -7,11 +7,8 @@ import { FaYoutube, FaInstagram } from "react-icons/fa6";
 import ParticleBackground from "../components/ParticlesBackground";
 
 const socials = [
-  { Icon: FaYoutube, label: "YouTube", href: "https://www.youtube.com/@gauravbitss" },
-  { Icon: FaXTwitter, label: "X", href: "https://x.com/gauravbuilds" },
-  { Icon: FaLinkedinIn, label: "LinkedIn", href: "https://www.linkedin.com/in/gaurav-gupta-4179671b0/" },
-  { Icon: FaInstagram, label: "Instagram", href: "https://www.instagram.com/gauravbits/" },
-  { Icon: FaGithub, label: "GitHub", href: "https://github.com/gauravgupta364" },
+  { Icon: FaLinkedinIn, label: "LinkedIn", href: "https://www.linkedin.com/in/sai-chandra-vinnakota/" },
+  { Icon: FaGithub, label: "GitHub", href: "https://github.com/saichandrav" },
 ];
 
 
